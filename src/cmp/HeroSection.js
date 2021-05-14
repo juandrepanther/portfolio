29a64a6f04dab06b7b298a12117759e47a3efca9
juandrepanther <janis@innergang.com>
2021-05-15 01:11:07 +0300

    edited video performance

--- src/cmp/HeroSection.js
+++ src/cmp/HeroSection.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import './HeroSection.css'
-import video1 from '../vid/1.mp4'
+import video1 from '../vid/2.mp4'
 import Button from './Button'
 
 function HeroSection() {
   return (
     <div className='hero-container'>
+      <div className='hero-overlay'></div>
       <video src={video1} autoPlay loop muted />
       <h1>ADVENTURES</h1>
       <p>What are you waiting for?</p>
