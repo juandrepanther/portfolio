dc7729c3405665b160c999567914cf99f2a40267
juandrepanther <janis@innergang.com>
2021-05-20 17:49:36 +0300

    svg file a bit eddited

--- src/cmp/pages/SvgTest.js
+++ src/cmp/pages/SvgTest.js
@@ -2,18 +2,19 @@ import React, {Component} from "react";
 import './SvgTest.css'
 import svg1 from '../../img/gears.svg'
 import Vivus from 'vivus'
-
-
-
 class SvgTest extends Component {
-
     componentDidMount() {
-        new Vivus('gears', {duration: 200, file: svg1})
+        new Vivus('gears', {
+            duration: 200, 
+            file: svg1,
+            animTimingFunction: Vivus.EASE_OUT
+        
+        })
     }
 
     render() {
         return (
-            <div>
+            <div className='svgAnime'>
                 <div id="gears"></div>
             </div>
         );
