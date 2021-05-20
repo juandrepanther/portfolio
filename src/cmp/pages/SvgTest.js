e9acdca899c3fb663c92557600498f9be5a684fd
juandrepanther <janis@innergang.com>
2021-05-20 19:40:03 +0300

    working with second animation SVG and added some paragrapghs

--- src/cmp/pages/SvgTest.js
+++ src/cmp/pages/SvgTest.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import './SvgTest.css'
 import svg1 from '../../img/gears.svg'
+import svg2 from '../../img/man.svg'
 import Vivus from 'vivus'
 class SvgTest extends Component {
     componentDidMount() {
@@ -8,14 +9,25 @@ class SvgTest extends Component {
             duration: 200, 
             file: svg1,
             animTimingFunction: Vivus.EASE_OUT
-        
+        })
+        new Vivus('man', {
+            duration: 200, 
+            file: svg2,
+            animTimingFunction: Vivus.EASE_OUT
         })
     }
+    
 
     render() {
         return (
             <div className='svgAnime'>
                 <div id="gears"></div>
+                    <section>
+                       <p>Time is our privilegy</p> 
+                       <p>Your problems are our problems</p> 
+                       <p>Its time to cooperate</p> 
+                    </section>
+                <div id="man"></div>
             </div>
         );
     }
