122ea6fc1153ac2d74d922fef6c6a9ab2945b5c5
juandrepanther <janis@innergang.com>
2021-05-29 22:03:48 +0300

    added bootstrap for playing a round

--- src/cmp/pages/Products.js
+++ src/cmp/pages/Products.js
@@ -1,12 +1,36 @@
 import React from 'react'
 import '../../App.css'
 import SvgPage from './SvgTest'
+import img1 from '../../img/houses/1h.jpg'
+import img2 from '../../img/houses/2h.jpg'
+import img3 from '../../img/houses/3h.jpg'
 
 function Products() {
     return (
         <>
         <h1 className='products'>PRODUCTS</h1>
         <SvgPage />
+        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src={img1} class="d-block w-100" alt="..." />
+    </div>
+    <div class="carousel-item">
+      <img src={img2} class="d-block w-100" alt="..." />
+    </div>
+    <div class="carousel-item">
+      <img src={img3} class="d-block w-100" alt="..." />
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
         </>
     )
 }
